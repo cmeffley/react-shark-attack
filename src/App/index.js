1796ee0e5e29fa615cd8b313d31cc0ea41bff94e
Chris Meffley <cmeffley@gmail.com>
2021-05-04 12:11:23 -0400

    finished styling

--- src/App/index.js
+++ src/App/index.js
@@ -25,20 +25,31 @@ function App() {
 
   return (
     <div className='App'>
-      {image ? <Button size='lg' color='warning' onClick={() => setImage(false)}>IT&apos;S SAFE, Everyone Back in the Water</Button>
-        : <Button
-        className='shark-btn'
-        color='danger'
-        onClick={handleClick}
-        disabled={liveStudents.length <= 0}
-      >Careful, there could be a <strong>SHARK ATTACK!</strong>
-      </Button>}
-    <div className='sharkImage'>
-      {image && <img src='https://www.surfertoday.com/images/stories/greatwhiteshark7.jpg' alt='Shark Attack' />}
+    <div>
+      {liveStudents.length <= 0 && <img src='https://images-na.ssl-images-amazon.com/images/I/61LgL555wUL._AC_SY679_.jpg' alt='Beach Closed' />}
     </div>
-      <h2><strong>SHARK TANK</strong></h2>
+      {image
+        ? <Button
+            id='safe'
+            size='lg'
+            color='warning'
+            onClick={() => setImage(false)}
+            disabled={liveStudents.length <= 0}>
+            IT&apos;S SAFE, Everyone Back in the Water</Button>
+        : <Button
+            className='shark-btn'
+            color='danger'
+            onClick={handleClick}
+            disabled={liveStudents.length <= 0}>
+            Careful, there could be a <strong>SHARK ATTACK!</strong>
+          </Button>
+      }
+    <div className='sharkImage'>
+      {liveStudents.length <= 0 ? '' : image && <img src='https://www.surfertoday.com/images/stories/greatwhiteshark7.jpg' alt='Shark Attack' />}
+    </div>
+      {/* <h2><strong>SHARK TANK</strong></h2> */}
       <SharkTank liveStudents={liveStudents}/><br />
-      <h2 id='graves'><strong>GRAVE YARD</strong></h2>
+      {/* <h2 id='graves'><strong>GRAVE YARD</strong></h2> */}
       <GraveYard deadStudents={deadStudents}/>
     </div>
   );
